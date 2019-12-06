ea8fe3532ddd7b36e8bcee1047a16417e7a155d8
lavana555 <timofeevniki3@gmail.com>
2019-12-06 20:26:33 +0300

    init commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classes from './Header.module.css'
+
+const Header=()=> {
+ return (
+
+    <header className={classes.header}>
+    <img src='https://i.pinimg.com/236x/2a/ca/23/2aca2374046eaf3846684ffea5b80fc3.jpg' />
+  </header>
+
+
+ )
+
+ }
+
+
+ export default Header;
---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import classes from './Post.module.css'
+
+const Post = () => {
+    return (
+
+
+        <div className={classes.item}>
+            <img src="https://i.pinimg.com/236x/61/7c/08/617c08db66249363967838302881b351.jpg" alt="" />
+            Post1
+                    <div>
+                <span>like</span>
+            </div>
+        </div>
+
+
+
+
+
+    )
+
+}
+
+
+export default Post;
